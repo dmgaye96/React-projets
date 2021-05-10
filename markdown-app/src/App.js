7b76b562c738550ec6b9d9b99050b72142b1073f
Doudou Mohamet GAYE <doudoumohametgaye@gmail.com>
2021-05-10 03:44:22 +0000

    markdown ok

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import marked from 'marked'
+import { sampleText } from './sampleText'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        text: sampleText
+    }
+
+    componentDidMount() {
+        const text = localStorage.getItem('text')
+        if (text) {
+            this.setState({ text })
+        } else {
+            this.setState({ text: sampleText })
+        }
+
+    }
+
+    componentDidUpdate() {
+        const { text } = this.state
+        localStorage.setItem('text', text)
+    }
+
+    handleChange = event => {
+        const text = event.target.value
+        this.setState({ text })
+
+    }
+
+    renderText = text => {
+        const __html = marked(text, { sanitize: true })
+        return { __html: __html }
+    }
+
+    render() {
+        return ( <
+            div className = "container" >
+            <
+            div className = "row" >
+            <
+            div className = "col-sm-6" >
+            <
+            textarea rows = "35"
+            className = "form-control"
+            onChange = { this.handleChange }
+            value = { this.state.text } >
+
+            <
+            /textarea> </div > <
+            div className = "col-sm-6" >
+            <
+            div dangerouslySetInnerHTML = { this.renderText(this.state.text) } >
+            <
+            /div>   < /
+            div > <
+            /div >  < /
+            div >
+        )
+    }
 }
 
 export default App;
